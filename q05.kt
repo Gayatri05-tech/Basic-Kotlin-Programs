de00d635969e5364f41ef6a44ebfead1d1dd7266
Gayatri05-tech <gayatriranidas2006@gmail.com>
2026-05-28 14:43:17 +0530

    Create q05.kt

---
+++ q05.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var num=1234
+    var temp=num
+    var reverse=0
+    while(temp!=0) 
+    {
+        val digit=temp % 10
+        reverse=reverse*10+digit
+        temp/=10
+    }
+    if(num==reverse)
+    println("Palindrome")
+    else
+        println("Not Palindrome")
+}
