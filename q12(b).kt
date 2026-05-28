85cbf98a7e02b8634f7f36d58aff05e7bbbae8cb
Gayatri05-tech <gayatriranidas2006@gmail.com>
2026-05-28 15:01:03 +0530

    Create q12(b).kt

---
+++ q12(b).kt
@@ -0,0 +1,10 @@
+//Swap two numbers without using third variable
+fun main() {
+    var a = 10
+    var b = 20
+    a = a + b
+    b = a - b
+    a = a - b
+    println("a = $a")
+    println("b = $b")
+}
