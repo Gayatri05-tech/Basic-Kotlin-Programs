abe57bd280da97fec462f526e406cbd3410a3c33
Gayatri05-tech <gayatriranidas2006@gmail.com>
2026-05-28 14:56:19 +0530

    Create q10.kt

---
+++ q10.kt
@@ -0,0 +1,11 @@
+//Function to check whether a character is vowel or consonant
+fun main() {
+    val ch = 'a'
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        println("$ch is a Vowel")
+    } 
+    else {
+        println("$ch is a Consonant")
+    }
+}
